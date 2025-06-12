e99095a975728885726fe0d3ad93f8172179ab36
shahbhavya7 <146073814+shahbhavya7@users.noreply.github.com>
2025-06-12 18:32:10 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import validators,streamlit as st
+from langchain.prompts import PromptTemplate
+from langchain_groq import ChatGroq
+from langchain.chains.summarize import load_summarize_chain
+from langchain_community.document_loaders import YoutubeLoader,UnstructuredURLLoader
+
+
+st.set_page_config(page_title="LangChain: Summarize Text From YT or Website", page_icon="🦜")
+st.title("🦜 LangChain: Summarize Text From YT or Website")
+st.subheader('Summarize URL')
+
+with st.sidebar:
+    groq_api_key=st.text_input("Groq API Key",value="",type="password")
